133fadb95099654add1123dc6a42d6d121dc3d68
Mohd Faiz <mohdfaiz.1842@gmail.com>
2025-06-26 15:46:46 +0530

    add ur;

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ if (process.env.mode === 'production') {
     app.use(cors())
 } else {
     app.use(cors({
-        origin: ["http://localhost:5174", "http://localhost:5173","https://ownifie.netlify.app"],
+        origin: ["http://localhost:5174", "http://localhost:5173","https://ownifie.netlify.app","https://adminownifie.netlify.app"],
         credentials: true,
     }))
 }
